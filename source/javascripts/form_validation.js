1980a83c7809754687dc7f95afa905df5da82d9d
Luuk Veenis <luukveenis@gmail.com>
2016-09-17 17:38:09 -0700

    Add better validation handling for all fields
    - bind the submit handler in external JS instead of inline
    - don't use pseudo element for error messages
    - dynamically add error messages to error field

--- source/javascripts/form_validation.js
+++ source/javascripts/form_validation.js
@@ -3,26 +3,42 @@
  * The code can be found here:
  * http://www.tutorialspoint.com/javascript/javascript_form_validations.htm
  */
-function validateGeneralForm() {
-  nameField = document.generalForm.name;
-  emailField = document.generalForm.email;
-  messageField = document.generalForm.message;
-  noErrors = true;
+window.FormValidations = {
 
-  if(nameField.value.length < 3) {
-    nameField.className += " form-field-error";
-    noErrors = false;
-  }
+  /* Function that validates general contact form fields: name, email and message */
+  validateGeneralForm: function(event) {
+    form = event.target;
+    nameField = form.name;
+    emailField = form.email;
+    messageField = form.message;
+    noErrors = true;
 
-  if(emailField.value == "") {
-    emailField.className += " form-field-error";
-    noErrors = false;
-  }
+    if(nameField.value.length < 3) {
+      nameField.className += " form-field-error";
+      nameField.nextElementSibling.innerHTML = "Please provide your full first and last name";
+      noErrors = false;
+    }
 
-  if(messageField.value == "")
-  {
-    messageField.className += " form-field-error";
-    noErrors = false;
+    if(emailField.value == "") {
+      emailField.className += " form-field-error";
+      emailField.nextElementSibling.innerHTML = "Please provide your email address";
+      noErrors = false;
+    }
+
+    if(messageField.value == "")
+    {
+      messageField.className += " form-field-error";
+      messageField.nextElementSibling.innerHTML = "Please tell us how we can help you";
+      noErrors = false;
+    }
+    event.preventDefault;
+    return noErrors;
   }
-  return noErrors;
 }
+
+/* Binds a listener to the submit event on the general contact form, which
+ * then calls our validator when the form is submitted.
+ */
+document.querySelector("#generalForm").addEventListener("submit", function(event) {
+  FormValidations.validateGeneralForm(event);
+});
