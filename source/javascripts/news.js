c7cff6ce6f2f7c7107954e6a7f7eef36dcd5f837
Luuk Veenis <luukveenis@gmail.com>
2016-10-23 21:15:33 -0700

    Add templates for creating news feed

---
+++ source/javascripts/news.js
@@ -0,0 +1,12 @@
+/* Author: Luuk Veenis
+ * Date: October 23, 2016
+ *
+ * This module is used to retrieve news for Counter Strike from Valve's web
+ * API. It handles the AJAX requests to the server, as well as parsing it and
+ * displaying the response on the page
+ */
+$(function() {
+  function NewsItem(response) {
+    // build the news item using the API response
+  }
+});
